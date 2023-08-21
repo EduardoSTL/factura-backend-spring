dde38e0d951e9ab09924cb8e13763ee664316d6b
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-08-21 16:27:12 -0600

    Initial commit

---
+++ src/main/java/org/eduardo/facturabackendspring/FacturaBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package org.eduardo.facturabackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eduardo/facturabackendspring/FacturaBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eduardo.facturabackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
