5c2e427bd9b50fb0ae545e45e92c040c2a3f5e94
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-08-22 16:19:26 -0600

    added: Categoria

---
+++ src/main/java/org/eduardo/facturabackendspring/models/entities/Categoria.java
@@ -0,0 +1,20 @@
+package org.eduardo.facturabackendspring.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "categoria")
+    private String nombre;
+}
